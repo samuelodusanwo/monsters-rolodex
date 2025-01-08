0076827d07d4c71bcd1a7f5f7f586b03a5a76201
youngsal <waleola805@gmail.com>
2025-01-08 22:12:56 +0100

    first react setup

--- src/App.js
+++ src/App.js
@@ -7,8 +7,23 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Samuel, We are about to start a big project
         </p>
+        <p>
+          My first React script
+        </p>
+        <h1>
+          Heading 1
+        </h1>
+        <p>
+          Hello Samuel, We are about to start a big project
+        </p>
+        <p>
+          My first React script
+        </p>
+        <h1>
+          Heading 1
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
