db3a1357e72a60657b2b0c426e1dbbb8ac3773a7
youngsal <waleola805@gmail.com>
2025-01-09 23:35:14 +0100

    keep pushing

--- src/App.js
+++ src/App.js
@@ -48,6 +48,92 @@ function App() {
         >
           Learn React
         </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Samuel, We are about to start a big project called tesema_Limited
+        </p>
+        <h1>
+          List of tesemaLimited names
+        </h1>
+        <ol>
+          <li>Tesema-Hub</li>
+          <li>Tesema-Tech</li>
+          <li>Tesema-Saloon</li>
+          <li>Tesema-Catery</li>
+          <li>Tesema-Housings</li>
+        </ol>
+        <ul>
+          <li>Tesema-Hub</li>
+          <li>Tesema-Tech</li>
+          <li>Tesema-Saloon</li>
+          <li>Tesema-Catery</li>
+          <li>Tesema-Housings</li>
+        </ul>
+        <ul>
+          <li>Tesema-Hub</li>
+          <li>Tesema-Tech</li>
+          <li>Tesema-Saloon</li>
+          <li>Tesema-Catery</li>
+          <li>Tesema-Housings</li>
+        </ul>
+        <p>
+          My first React script
+        </p>
+        <h1>
+          Subtitle
+        </h1>
+        <h2>Jesus you are LORD</h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Samuel, We are about to start a big project called tesema_Limited
+        </p>
+        <h1>
+          List of tesemaLimited names
+        </h1>
+        <ol>
+          <li>Tesema-Hub</li>
+          <li>Tesema-Tech</li>
+          <li>Tesema-Saloon</li>
+          <li>Tesema-Catery</li>
+          <li>Tesema-Housings</li>
+        </ol>
+        <ul>
+          <li>Tesema-Hub</li>
+          <li>Tesema-Tech</li>
+          <li>Tesema-Saloon</li>
+          <li>Tesema-Catery</li>
+          <li>Tesema-Housings</li>
+        </ul>
+        <ul>
+          <li>Tesema-Hub</li>
+          <li>Tesema-Tech</li>
+          <li>Tesema-Saloon</li>
+          <li>Tesema-Catery</li>
+          <li>Tesema-Housings</li>
+        </ul>
+        <p>
+          My first React script
+        </p>
+        <h1>
+          Subtitle
+        </h1>
+        <h2>Jesus you are LORD</h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
