c91a753f706c719437f5247f2bdc19ba4388631f
youngsal <waleola805@gmail.com>
2025-01-13 12:35:35 +0100

    not real code

--- src/App.js
+++ src/App.js
@@ -20,20 +20,23 @@ class App extends Component {
           name: 'Zombie',
           id: 'mon3'
         }
-      ]
+      ],
+      string : "Hello great man"
     }
   }
 
   render () {
     return (
       <div className="App">
-        <header className="App-header">
+        <header classN ame="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Hello Samuel, we are about to start a big project called TesemaLimited</p>
 
+          <button onClick={() => this.setState({string: "I'm great"})}>Click Me</button>
           {
             this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
           }
+          <p>{this.state.string}</p>
         </header>
       </div>
     )
