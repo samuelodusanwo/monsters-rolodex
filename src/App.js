4254c1d0df5f61594359b361df2e4f18a0bbb601
youngsal <waleola805@gmail.com>
2025-01-14 09:28:14 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -44,6 +44,11 @@ class App extends Component {
                 monster => <h1 key={monster.id}>{monster.name}</h1>
               )
             }
+            {
+              this.state.monsters.map(
+                monster => <h1 key={monster.id}>{monster.name}</h1>
+              )
+            }
           <p>{this.state.string}</p>
         </header>
       </div>
