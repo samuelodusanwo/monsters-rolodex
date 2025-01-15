804dce7562fe85f50db5128197fc51d6829a74aa
youngsal <waleola805@gmail.com>
2025-01-15 12:48:56 +0100

    initial commit: error code

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { CardList } from './components/card-lists/card-list.component'
+import { CardList } from './components/card-lists/card-list.component';
 
 class App extends Component {
   constructor() {
     super()
 
     this.state = {
-      data: [],
       string: 'Hello Samuel Odusanwo',
-      monster: []
+      monsters: []
     }
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(data => this.setState({data}))
+      .then(monsters => this.setState({monsters}))
   }
 
   render () {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" />
           <p>Hello Samuel Odusanwo, we are about to start a big project called TesemaLimited</p>
           <button onClick={() => this.setState({string: "I'm Great"})}>Click Me</button>
-          <p>{this.state.string}</p>
-          <CardList>
-            {
-              this.state.data.map(
-                user => <h1 key={user.id}>{user.name}</h1>
-              )
-            }
-          </CardList>
+          <p>{this.state.string}</p> */}
+          <CardList monsters={this.state.monsters} />
         </header>
       </div>
     )
