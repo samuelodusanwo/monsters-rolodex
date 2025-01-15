19534e996694adf0919dbbdbe1877e0242a419ed
youngsal <waleola805@gmail.com>
2025-01-15 16:30:05 +0100

    initial error

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import './card.styles.css';
 
 export const Card = (props) => (
-    <div>
+    <div className='card-container'>
+        <img src={`https://robohash.org/${props.monster.id}?set=set2`} alt="monster" />
         <h1>{props.monster.name}</h1>
     </div>
 );
