a49fc518c6758a6c87d81b325993e241525e1d52
youngsal <waleola805@gmail.com>
2025-01-15 10:22:13 +0100

    pushing error code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-lists/card-list.component'
 
 class App extends Component {
-  constructor (){
+  constructor() {
     super()
 
     this.state = {
       data: [],
-      string : 'Samuel the Tech Lord'
+      string: 'Hello Samuel Odusanwo'
     }
   }
 
@@ -17,22 +18,22 @@ class App extends Component {
       .then(response => response.json())
       .then(data => this.setState({data}))
   }
-  
+
   render () {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Hello Samuel Odusanwo, we are about to start a big project called TesemaLimited</p>
-
-          <button onClick={() => this.setState({string: "I'm super Great"})}>Click Me</button>
+          <button onClick={() => this.setState({string: "I'm Great"})}>Click Me</button>
           <p>{this.state.string}</p>
-
-         {
-          this.state.data.map(
-            user => <h1 key={user.id}>{user.name}</h1>
-          )
-         }
+          <CardList>
+            {
+              this.state.data.map(
+                user => <h1 key={user.id}>{user.name}</h1>
+              )
+            }
+          </CardList>
         </header>
       </div>
     )
