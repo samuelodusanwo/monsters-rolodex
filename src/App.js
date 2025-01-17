e227c378005ec98cb7c965a7610192b4ae1c4b9f
youngsal <waleola805@gmail.com>
2025-01-17 13:09:31 +0100

    initial commit: push first react code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 // import logo from './logo.svg';
 import {CardList} from './components/card-lists/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -32,7 +33,11 @@ class App extends Component {
           <h4>Hello Samuel, We are about to start a big Project for the year</h4>
           <p>{this.state.string}</p>
           <button onClick={() => this.setState({string: 'And the best Hacker of the year 2025!'})}>Click me</button>
-          <input type="search" placeholder="Search monsters" onChange={e => this.setState({searchField: e.target.value})}/>
+          {/* <input type="search" placeholder="Search monsters" onChange={e => this.setState({searchField: e.target.value})}/> */}
+          <SearchBox
+            placeholder='Search monsters'
+            handleChange={e => this.setState({searchField: e.target.value})}
+          />
           <CardList monsters={filteredMonsters}/>
         </header>
       </div>
