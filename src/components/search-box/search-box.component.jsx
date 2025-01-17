efc086f3efd01c1ed3a67344748fd1d7d1cb7879
youngsal <waleola805@gmail.com>
2025-01-17 13:12:52 +0100

    initial commit: push first react code

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import './search-box.styles.css';
 
 export const SearchBox = ({placeholder, handleChange}) => (
-    <input
-        className='search'
+    <input 
         type="search"
         placeholder={placeholder}
         onChange={handleChange}
