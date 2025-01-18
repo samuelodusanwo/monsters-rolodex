91350bf8eccc0b636fb2c3e1049dcac19b4a4d0a
youngsal <waleola805@gmail.com>
2025-01-18 15:16:39 +0100

    initial commit: push first react code

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,5 +6,6 @@ export const Card = (props) => (
         <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster" />
         <h3>{props.monster.name}</h3>
         <small>{props.monster.email}</small>
+        <h1>{props.monster.username}</h1>
     </div>
 )
